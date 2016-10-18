e4b87477bf126bb70740d8e165c3c78ace4a42ff
Sebastien Routier <sroutier@gmail.com>
2016-10-18 13:35:57 -0400

    Change settings to follow convention.

--- config/config.php
+++ config/config.php
@@ -9,8 +9,8 @@ return [
     | An example of a config setting with it's value.
     |
     */
-// TODO: RPT theme
-    'config_setting' => env('reports_users.CONFIG_SETTING', 'default_value'),
+    // TODO: RPT theme
+    'config_setting' => env('MODULES.REPORTS_USERS.CONFIG_SETTING', 'default_value'),
 
 ];
 
